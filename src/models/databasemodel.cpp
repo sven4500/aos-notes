96da885c7fd81c6d72a006da7cb712de64debde1
Ivars Rozhleys <sven4500@mail.ru>
2024-09-06 00:52:35 +0300

    Add database model

---
+++ src/models/databasemodel.cpp
@@ -0,0 +1,53 @@
+#include <QDebug>
+#include <QSqlDatabase>
+#include <QSqlError>
+#include <QSqlQuery>
+#include <QStandardPaths>
+
+#include "databasemodel.h"
+
+namespace Models {
+
+QString const DatabaseModel::DriverName = "QSQLITE";
+QString const DatabaseModel::DatabaseFilename = "notes.sqlite";
+
+DatabaseModel::DatabaseModel(QObject *parent)
+    : QObject(parent)
+{
+    if (!QSqlDatabase::isDriverAvailable(DriverName))
+        throw std::runtime_error(QString("no provider exists for %1").arg(DriverName).toStdString());
+
+    // INFO: https://stackoverflow.com/questions/2241808/checking-if-a-folder-exists-and-creating-folders-in-qt-c
+    QDir const workingDir = QStandardPaths::writableLocation(QStandardPaths::DocumentsLocation) +
+            QDir::separator() + "Notes";
+    QDir().mkdir(workingDir.path());
+
+    m_database = QSqlDatabase::addDatabase(DriverName);
+    m_database.setDatabaseName(workingDir.filePath(DatabaseFilename));
+
+    if (!m_database.open())
+        throw std::runtime_error(QString("failed to open database at location %1").arg(workingDir.path()).toStdString());
+
+    // INFO: https://stackoverflow.com/questions/9342249/how-to-insert-a-unique-id-into-each-sqlite-row
+    // INFO: https://stackoverflow.com/questions/62434913/how-to-create-a-table-in-qt-with-sqlite
+    QSqlQuery query(m_database);
+    query.exec("CREATE TABLE IF NOT EXISTS media (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, "
+        "type INTEGER, title TEXT, media TEXT, created DATETIME, modified DATETIME);");
+}
+
+void DatabaseModel::create(NoteType noteType, QString title, QString media)
+{
+    qDebug() << "create note" << title << noteType;
+
+    QSqlQuery query(m_database);
+    query.prepare("INSERT INTO media (type, title, media, created, modified) "
+        "VALUES (?, ?, ?, ?, ?)");
+    query.addBindValue(noteType);
+    query.addBindValue(title);
+    query.addBindValue(media);
+    query.addBindValue(QDateTime::currentDateTime());
+    query.addBindValue(QDateTime::currentDateTime());
+    query.exec();
+}
+
+}
---
+++ src/models/databasemodel.h
@@ -0,0 +1,43 @@
+#ifndef DATABASEMODEL_H
+#define DATABASEMODEL_H
+
+#include <QDir>
+#include <QObject>
+#include <QSqlDatabase>
+
+#include <optional>
+
+#include "dto/databaseentry.h"
+
+namespace Models {
+
+class DatabaseModel : public QObject
+{
+    Q_OBJECT
+
+public:
+    enum NoteType {
+        TextNote,
+        SketchNote,
+        AudioNote
+    };
+    Q_ENUM(NoteType)
+
+    explicit DatabaseModel(QObject *parent = nullptr);
+
+    void create(NoteType noteType, QString title, QString media);
+    std::optional<DatabaseEntry> find(qint64 id);
+    void update(qint64 id);
+    void erase(qint64 id);
+
+private:
+    static const QString DatabaseFilename;
+    static const QString DriverName;
+
+    QSqlDatabase m_database;
+
+};
+
+}
+
+#endif // DATABASEMODEL_H
