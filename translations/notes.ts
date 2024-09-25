741a4c761b5e085ca286e597824d641cd83c0ae4
Ivars Rozhleys <sven4500@mail.ru>
2024-09-25 21:50:08 +0300

    Update translations;
    - add Russian translation

--- translations/notes.ts
+++ translations/notes.ts
@@ -2,49 +2,35 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="en">
 <context>
-    <name>AboutPage</name>
+    <name>ApplicationHeader</name>
     <message>
-        <location filename="../qml/pages/AboutPage.qml" line="58"/>
-        <source>About Application</source>
-        <translation>About Application</translation>
-    </message>
-    <message>
-        <location filename="../qml/pages/AboutPage.qml" line="68"/>
-        <source>#descriptionText</source>
-        <translation>&lt;p&gt;Приложение для ведения текстовых, аудио и визуальных заметок для ОС Аврора&lt;/p&gt;</translation>
-    </message>
-    <message>
-        <location filename="../qml/pages/AboutPage.qml" line="73"/>
-        <source>3-Clause BSD License</source>
-        <translation>3-Clause&#xa0;BSD License</translation>
-    </message>
-    <message>
-        <location filename="../qml/pages/AboutPage.qml" line="83"/>
-        <source>#licenseText</source>
-        <translation>&lt;p&gt;&lt;i&gt;Copyright&#xa0;(C)&#xa0;2022 ru.ivars.rozhleys&lt;/i&gt;&lt;/p&gt;
-&lt;p&gt;Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:&lt;/p&gt;
-&lt;ol&gt;
-	&lt;li&gt;Redistributions of&#xa0;source code must retain the above copyright notice, this list of&#xa0;conditions and the following disclaimer.&lt;/li&gt;
-	&lt;li&gt;Redistributions in&#xa0;binary form must reproduce the above copyright notice, this list of&#xa0;conditions and the following disclaimer in&#xa0;the documentation and/or other materials provided with the distribution.&lt;/li&gt;
-	&lt;li&gt;Neither the name of&#xa0;the copyright holder nor the names of&#xa0;its contributors may be&#xa0;used to&#xa0;endorse or&#xa0;promote products derived from this software without specific prior written permission.&lt;/li&gt;
-&lt;/ol&gt;
-&lt;p&gt;THIS SOFTWARE IS&#xa0;PROVIDED BY&#xa0;THE&#xa0;COPYRIGHT HOLDERS AND CONTRIBUTORS &quot;AS&#xa0;IS&quot; AND ANY EXPRESS OR&#xa0;IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE&#xa0;IMPLIED WARRANTIES OF&#xa0;MERCHANTABILITY AND FITNESS FOR A&#xa0;PARTICULAR PURPOSE ARE DISCLAIMED. IN&#xa0;NO&#xa0;EVENT SHALL THE&#xa0;COPYRIGHT HOLDER OR&#xa0;CONTRIBUTORS BE&#xa0;LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR&#xa0;CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF&#xa0;SUBSTITUTE GOODS OR&#xa0;SERVICES; LOSS OF&#xa0;USE, DATA, OR&#xa0;PROFITS; OR&#xa0;BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON&#xa0;ANY THEORY OF&#xa0;LIABILITY, WHETHER IN&#xa0;CONTRACT, STRICT LIABILITY, OR&#xa0;TORT (INCLUDING NEGLIGENCE OR&#xa0;OTHERWISE) ARISING IN&#xa0;ANY WAY OUT OF&#xa0;THE&#xa0;USE OF&#xa0;THIS SOFTWARE, EVEN IF&#xa0;ADVISED OF&#xa0;THE&#xa0;POSSIBILITY OF&#xa0;SUCH DAMAGE.&lt;/p&gt;</translation>
-    </message>
-</context>
-<context>
-    <name>DefaultCoverPage</name>
-    <message>
-        <location filename="../qml/cover/DefaultCoverPage.qml" line="47"/>
+        <location filename="../qml/elements/ApplicationHeader.qml" line="18"/>
         <source>Notes</source>
         <translation>Notes</translation>
     </message>
 </context>
 <context>
-    <name>MainPage</name>
+    <name>NoteHeader</name>
     <message>
-        <location filename="../qml/pages/MainPage.qml" line="48"/>
-        <source>Notes</source>
-        <translation>Notes</translation>
+        <location filename="../qml/elements/NoteHeader.qml" line="51"/>
+        <source>Note title...</source>
+        <translation>Note title...</translation>
+    </message>
+</context>
+<context>
+    <name>NoteListPage</name>
+    <message>
+        <location filename="../qml/pages/NoteListPage.qml" line="25"/>
+        <source>No notes have been made at this time. Click any button below to create a note.</source>
+        <translation>No notes have been made at this time. Click any button below to create a note.</translation>
+    </message>
+</context>
+<context>
+    <name>TextNotePage</name>
+    <message>
+        <location filename="../qml/pages/TextNotePage.qml" line="52"/>
+        <source>Note text...</source>
+        <translation>Note text...</translation>
     </message>
 </context>
 </TS>
